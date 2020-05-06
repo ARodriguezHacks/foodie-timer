02e7dd5b22b5c136b4b637686fab129946f497bc
Angie Rodriguez <angrodri902@gmail.com>
2020-05-06 16:28:02 -0700

    Implement change events for settings input options

--- timer-settings.js
+++ timer-settings.js
@@ -1,6 +1,9 @@
 let settingsTrigger = document.querySelector(".settings-trigger");
 let settingsModal = document.querySelector(".settings-modal");
-let settingsWrapper = document.querySelector(".settings-dropdown-wrapper");
+let settingsOptions = document.querySelectorAll(".settings-option");
+let currentSettings = document.querySelectorAll(".current");
+currentSettings = Array.from(currentSettings);
+
 let foodieBreak = document.querySelector(".foodie-opt");
 let snackBreak = document.querySelector(".snack-opt");
 
@@ -9,15 +12,29 @@ settingsTrigger.addEventListener("click", function () {
   settingsModal.classList.toggle("open");
 });
 
-//Need to fix form element in HTML. Remove it and replace it where its parent element is not the settings icon.
+for (const option of settingsOptions) {
+  option.addEventListener("change", function (e) {
+    const parent = option.parentNode;
+    let currentItem = currentSettings.find((elem) => {
+      return elem.classList.contains(e.target.name);
+    });
+    if (e.target.value.length < 2) {
+      currentItem.textContent = "0" + `${e.target.value}`;
+    } else {
+      currentItem.textContent = e.target.value;
+    }
+  });
+}
+
+function changeMinsSettings() {}
 
 // window.addEventListener("click", function (e) {
 //   console.log(e.target);
-//   console.log(settingsModal.contains(e.target));
-//   // const select = document.querySelector(".settings-dropdown");
-//   // if (!select.contains(e.target)) {
-//   //settingsModal.classList.remove("open");
-//   //}
+//   console.log(settingsModal.classList.contains("open"));
+//   //const wrapper = document.querySelector(".custom-dropdown-wrapper");
+//   if (!settingsModal.contains(e.target)) {
+//     settingsModal.classList.remove("open");
+//   }
 // });
 
 export { settingsTrigger, settingsModal, foodieBreak, snackBreak };
