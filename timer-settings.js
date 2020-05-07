a279ed01a3b0c1596b6ffcfcbce98c466ee9cdba
Angie Rodriguez <angrodri902@gmail.com>
2020-05-06 18:09:31 -0700

    Add click handler when settings modal inactive

--- timer-settings.js
+++ timer-settings.js
@@ -2,13 +2,13 @@ let settingsTrigger = document.querySelector(".settings-trigger");
 let settingsModal = document.querySelector(".settings-modal");
 let settingsOptions = document.querySelectorAll(".settings-option");
 let currentSettings = document.querySelectorAll(".current");
+
 currentSettings = Array.from(currentSettings);
 
 let foodieBreak = document.querySelector(".foodie-opt");
 let snackBreak = document.querySelector(".snack-opt");
 
 settingsTrigger.addEventListener("click", function () {
-  //console.log(e.target);
   settingsModal.classList.toggle("open");
 });
 
@@ -26,15 +26,15 @@ for (const option of settingsOptions) {
   });
 }
 
-function changeMinsSettings() {}
-
-// window.addEventListener("click", function (e) {
-//   console.log(e.target);
-//   console.log(settingsModal.classList.contains("open"));
-//   //const wrapper = document.querySelector(".custom-dropdown-wrapper");
-//   if (!settingsModal.contains(e.target)) {
-//     settingsModal.classList.remove("open");
-//   }
-// });
+window.addEventListener("click", function (e) {
+  if (settingsModal.classList.contains("open")) {
+    if (
+      !settingsModal.contains(e.target) &&
+      !settingsTrigger.contains(e.target)
+    ) {
+      settingsModal.classList.remove("open");
+    }
+  }
+});
 
 export { settingsTrigger, settingsModal, foodieBreak, snackBreak };
